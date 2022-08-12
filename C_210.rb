e69a3a9345866e1a8d3643932840a3d7e7ca5384
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-12 09:12:00 +0900

    C_210fixed

--- C_210.rb
+++ C_210.rb
@@ -1,13 +1,18 @@
 n, k = gets.split.map &:to_i
 candy_colors = gets.split.map &:to_i
 all_combination = []
+candy_combination = candy_colors[0..(k-1)]
 (n - k + 1).times do |i|
-  uniq_candy_count = candy_colors[i..(i+k-1)].uniq.length
+  uniq_candy_count = candy_combination.uniq.length
   if uniq_candy_count == k
     puts k
     return
   end
-  all_combination << uniq_candy_count
+  all_combination << candy_combination.uniq.length
+  if i + k != n
+    candy_combination.delete_at(0)
+    candy_combination << candy_colors[i+k]
+  end
 end
  
-puts all_combination.maxtion.max
+puts all_combination.max
