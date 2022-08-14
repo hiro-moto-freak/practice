baab0c3d07ca2002d5c3816b6543696aacdf473a
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-14 11:10:13 +0900

    B_255

--- B_255.rb
+++ B_255.rb
@@ -1,28 +1,46 @@
 n, k = gets.split.map &:to_i
 A = gets.split.map &:to_i
-XY = n.times.map{gets.split.map &:to_i}
+all_people = n.times.map{gets.split.map &:to_i}
 
-people_with_rights = XY.combination(k).map &:to_a
-l = people_with_rights.length
+people_with_rights = []
+r = []
 
-ans = []
+A.each do |a|
+  people_with_rights << all_people[a - 1]
+end
 
-people_with_rights.each do |pwr|
-  people_without_rights = XY.dup
-  k.times do |i|
-    people_without_rights.delete_at(people_without_rights.index(pwr[i]))
+people_without_rights = all_people - people_with_rights
+
+people_without_rights.each do |pwor|
+  d = []
+  people_with_rights.each do |pwr|
+    d << (pwor[0]-pwr[0])**2 + (pwor[1]-pwr[1])**2 
   end
+  r << d.min
+end
+
+puts Math.sqrt(r.max)
+
+# l = people_with_rights.length
+
+# ans = []
+
+# people_with_rights.each do |pwr|
+#   people_without_rights = XY.dup
+#   k.times do |i|
+#     people_without_rights.delete_at(people_without_rights.index(pwr[i]))
+#   end
 
   
 
-  people_without_rights.each do |pwor|
-    distance = []
-    k.times do |j|
-      distance << ((pwor[0]-pwr[j][0])**2 + (pwor[1]-pwr[j][1])**2)
-    end
-    ans << Math.sqrt(distance.max)
-  end
-end
+#   people_without_rights.each do |pwor|
+#     distance = []
+#     k.times do |j|
+#       distance << ((pwor[0]-pwr[j][0])**2 + (pwor[1]-pwr[j][1])**2)
+#     end
+#     ans << Math.sqrt(distance.max)
+#   end
+# end
 
 
-p ans.max
+# p ans.max
