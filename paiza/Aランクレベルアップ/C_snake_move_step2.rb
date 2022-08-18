68abc2d95d38ec49c8925ed3ede9bb15d59c0d36
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-18 23:38:18 +0900

    C_snake_move_step1_as

--- paiza/Aランクレベルアップ/C_snake_move_step2.rb
+++ paiza/Aランクレベルアップ/C_snake_move_step2.rb
@@ -1,15 +1,25 @@
-y, x, n = gets.split.map(&:to_i)
-directions = n.times.map{gets.chomp}
+# y, x, n = gets.split.map(&:to_i)
+# directions = n.times.map{gets.chomp}
 
-directions.each do |d|
-  if d == "N"
-    y -= 1
-  elsif d == "S"
-    y += 1
-  elsif d == "E"
-    x += 1
-  else
-    x -= 1
-  end
-  puts "#{y} #{x}"
+# directions.each do |d|
+#   if d == "N"
+#     y -= 1
+#   elsif d == "S"
+#     y += 1
+#   elsif d == "E"
+#     x += 1
+#   else
+#     x -= 1
+#   end
+#   puts "#{y} #{x}"
+# end
+
+y, x, n = gets.split.map(&:to_i)
+move = {"N" => [-1, 0], "S" => [1, 0], "E" => [0, 1], "W" => [0, -1]}
+
+n.times do |i|
+  d = gets.chomp
+  y += move[d][0]
+  x += move[d][1]
+  puts y.to_s + " " + x.to_s
 end
