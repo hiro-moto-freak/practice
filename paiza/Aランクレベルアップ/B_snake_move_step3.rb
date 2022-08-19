e4af68b49ff2326c4f67869d136400e8ec38dbc6
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-19 13:04:14 +0900

    B_snake_move_step3

--- paiza/Aランクレベルアップ/B_snake_move_step3.rb
+++ paiza/Aランクレベルアップ/B_snake_move_step3.rb
@@ -1,19 +1,35 @@
-y, x, D = gets.split.map(&:to_s)
-d = gets.chomp.to_s
-y = y.to_i
-x = x.to_i
+# y, x, D = gets.split.map(&:to_s)
+# d = gets.chomp.to_s
+# y = y.to_i
+# x = x.to_i
 
-move = {
-  "N" => {"R" => 1, "L" => -1},
-  "S" => {"R" => -1, "L" => 1},
-  "E" => {"R" => 1, "L" => -1},
-  "W" => {"R" => -1, "L" => 1}
-}
+# move = {
+#   "N" => {"R" => 1, "L" => -1},
+#   "S" => {"R" => -1, "L" => 1},
+#   "E" => {"R" => 1, "L" => -1},
+#   "W" => {"R" => -1, "L" => 1}
+# }
 
-if D == "N" || D == "S"
-  x += move[D][d]
+# if D == "N" || D == "S"
+#   x += move[D][d]
+# else
+#   y += move[D][d]
+# end
+
+# puts y.to_s + " " + x.to_s
+
+y, x, D = gets.split.map.with_index{|x, i| i != 2 ? x.to_i : x.to_s}
+d = gets.chomp
+
+
+if D == "N"
+  d == "R" ? x += 1 : x -= 1
+elsif D == "S"
+  d == "R" ? x -= 1 : x += 1
+elsif D == "E"
+  d == "R" ? y += 1 : y -= 1
 else
-  y += move[D][d]
+  d == "R" ? y -= 1 : y += 1
 end
 
 puts y.to_s + " " + x.to_s
