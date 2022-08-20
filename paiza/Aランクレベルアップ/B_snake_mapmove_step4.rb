b503a98b5fc010dbf9f424328ac6f4af3bdd9909
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-20 13:40:10 +0900

    B_snake_mapmove_step4

--- paiza/Aランクレベルアップ/B_snake_mapmove_step4.rb
+++ paiza/Aランクレベルアップ/B_snake_mapmove_step4.rb
@@ -3,22 +3,27 @@ field = h.times.map{gets.chomp}
 
 directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 i = 0
+stop = false
 
 n.times do
   rl, num = gets.split.map.with_index{|val, i| i == 0 ? val.to_s : val.to_i}
   rl == "R" ? i += 1 : i -= 1
+  
   num.times do
-    y += directions[i % 4][0]
-    x += directions[i % 4][1]
-    if field[y][x] == nil || y < 0 || x < 0
-      y -= directions[i % 4][0]
-      x -= directions[i % 4][1]
-      puts y.to_s + " " + x.to_s
-      puts "Stop"
-      return
-    elsif field[y][x] == "#"
+    ny = y + directions[i % 4][0]
+    nx = x + directions[i % 4][1]
+    
+    if !(ny <= h - 1 && ny >= 0 && nx <= w - 1 && nx >= 0 && field[ny][nx] != "#")
+      stop = true
       break
     end
+    y, x = ny, nx
   end
+
   puts y.to_s + " " + x.to_s
+  
+  if stop
+      puts "Stop"
+      return
+  end
 end
