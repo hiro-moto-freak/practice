4b767d967f59e7c02bc7185d45b27e85884f3f8c
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-20 10:03:29 +0900

    B_snake_mapmove_step2_as

--- paiza/Aランクレベルアップ/B_snake_mapmove_step2.rb
+++ paiza/Aランクレベルアップ/B_snake_mapmove_step2.rb
@@ -1,17 +1,40 @@
+# h, w, y, x, d, m = gets.split.map.with_index{|val, i| (i == 4 || i == 5) ? val.to_s : val.to_i}
+# field = h.times.map{gets.chomp} 
+
+# direction = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+# if d == "N"
+#   i = 0
+# elsif d == "E"
+#   i = 1
+# elsif d == "S"
+#   i = 2
+# else
+#   i = 3
+# end
+
+# if m == "R"
+#   i += 1
+# else
+#   i -= 1
+# end
+
+# y += direction[i][0]
+# x += direction[i][1]
+
+# if field[y][x] == "#" || field[y][x] == nil || y < 0 || x < 0
+#   puts "No"
+# else
+#   puts "Yes"
+# end
+
 h, w, y, x, d, m = gets.split.map.with_index{|val, i| (i == 4 || i == 5) ? val.to_s : val.to_i}
 field = h.times.map{gets.chomp} 
 
+nesw = %w(N E S W)
 direction = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
-if d == "N"
-  i = 0
-elsif d == "E"
-  i = 1
-elsif d == "S"
-  i = 2
-else
-  i = 3
-end
+i = nesw.index(d)
 
 if m == "R"
   i += 1
