0e858e0c3aec09e2d174dc9a8f666cb73b26cf54
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-21 14:38:25 +0900

    B_twopointers_step2_fixed

--- paiza/Aランクレベルアップ/区間の積/B_twopointers_step2.rb
+++ paiza/Aランクレベルアップ/区間の積/B_twopointers_step2.rb
@@ -1,8 +1,25 @@
-N = gets.to_i
-nums = gets.split.map &:to_i
-n = gets.to_i
+# N = gets.to_i
+# nums = gets.split.map &:to_i
+# n = gets.to_i
 
-n.times do
+# n.times do
+#   f, l = gets.split.map &:to_i
+#   puts nums[f..l].sum 
+# end
+
+n = gets.to_i
+nums = gets.split.map &:to_i
+
+sums = []
+sum = 0
+n.times do |i|
+  sum += nums[i]
+  sums << sum
+end
+
+q = gets.to_i
+
+q.times do
   f, l = gets.split.map &:to_i
-  puts nums[f..l].sum 
+  puts sums[l] - sums[f] + nums[f]
 end
