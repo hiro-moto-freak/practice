f79895915e1d0656f2be35e1c587a359d6d0585b
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-22 10:58:22 +0900

    B_twopointers_step3_as

--- paiza/Aランクレベルアップ/区間の積/B_twopointers_step3.rb
+++ paiza/Aランクレベルアップ/区間の積/B_twopointers_step3.rb
@@ -20,34 +20,55 @@
 
 # puts -1
 
+# n, m = gets.split.map &:to_i
+# nums = gets.split.map &:to_i
+
+# head, last = 0, 0
+# sum = nums[0]
+# ans = n
+
+# if nums.sum < m
+#   puts -1
+#   return
+# end
+
+# loop do
+#   if sum < m
+#     last += 1
+#     break if last == n - 1
+#     sum += nums[last]
+#     if sum >= m
+#       ans = [ans, last-head+1].min
+#     end
+#   else
+#     sum -= nums[head]
+#     head += 1
+#     break if head == n
+#     if sum >= m
+#       ans = [ans, last-head+1].min
+#     end 
+#   end
+# end
+
+# puts ans
+
 n, m = gets.split.map &:to_i
 nums = gets.split.map &:to_i
 
-head, last = 0, 0
+first, last = 0, 0
 sum = nums[0]
-ans = n
-
-if nums.sum < m
-  puts -1
-  return
-end
+ans = n + 1
 
 loop do
   if sum < m
-    last += 1
     break if last == n - 1
+    last += 1
     sum += nums[last]
-    if sum >= m
-      ans = [ans, last-head+1].min
-    end
   else
-    sum -= nums[head]
-    head += 1
-    break if head == n
-    if sum >= m
-      ans = [ans, last-head+1].min
-    end 
+    ans = [ans, last - first + 1].min
+    sum -= nums[first]
+    first += 1
   end
 end
 
-puts ans
+puts ans == n + 1 ? -1 : ans
