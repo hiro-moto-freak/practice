f1189df197927fe108912088803dcc667e2a5e7e
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-22 12:46:14 +0900

    B_twopointers_step4

---
+++ paiza/Aランクレベルアップ/区間の積/B_twopointers_step4.rb
@@ -0,0 +1,20 @@
+n, m = gets.split.map &:to_i
+nums = gets.split.map &:to_i
+
+first, last = 0, 0
+sum = nums[0]
+ans = 0
+
+loop do
+  if sum <= m
+    ans = [0, last - first + 1].max
+    break if last == n - 1
+    last += 1
+    sum += nums[last]
+  elsif
+    sum -= nums[first]
+    first += 1
+  end
+end
+
+puts ans
