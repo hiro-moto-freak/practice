43434ce84ced08e1ea9614de0902ba36876dfa78
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-23 09:22:00 +0900

    A_twopointers_step5_fixed

--- paiza/Aランクレベルアップ/区間の積/A_twopointers_step5.rb
+++ paiza/Aランクレベルアップ/区間の積/A_twopointers_step5.rb
@@ -1,9 +1,39 @@
-n, m = gets.split.map &:to_i
+# n, m = gets.split.map &:to_i
+# nums = gets.split.map &:to_i
+
+# m.times do
+#   f, l, a = gets.split.map &:to_i
+#   nums.map!.with_index{|val, i| i >= f - 1 && i <= l - 1 ? val += a : val}
+# end
+
+# puts nums
+
+# n, m = gets.split.map &:to_i
+# nums = gets.split.map &:to_i
+
+# m.times do
+#   f, l, a = gets.split.map &:to_i
+#   while l >= f do
+#     nums[f - 1] += a
+#     f += 1
+#   end
+# end
+
+# puts nums
+
 nums = gets.split.map &:to_i
+
+add = Array.new(n + 1, 0)
 
 m.times do
   f, l, a = gets.split.map &:to_i
-  nums.map!.with_index{|val, i| i >= f - 1 && i <= l - 1 ? val += a : val}
+  add[f - 1] += a
+  add[l] -= a
+end
+
+n.times do |i|
+  nums[i] += add[i]
+  add[i + 1] += add[i]
 end
 
 puts nums
