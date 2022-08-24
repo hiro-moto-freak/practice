cce3eed56078b4bbf28493155e3177dc6849a054
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-24 17:32:18 +0900

    B_word_chain_as

--- paiza/スキルチェック過去問/B_word_chain.rb
+++ paiza/スキルチェック過去問/B_word_chain.rb
@@ -1,40 +1,79 @@
+# n, k, m = gets.split.map &:to_i
+# word_list = k.times.map{gets.chomp.to_s}
+# utterances = m.times.map{gets.chomp.to_s}
+
+# player = [*1..n]
+# dropout_time = []
+# previous_utter = []
+
+# m.times do |i|
+#   if word_list.include?(utterances[i]) && !(utterances[i].end_with?("z"))
+#     if previous_utter.include?(utterances[i])
+#       dropout_time << i + 1
+#     else
+#       previous_utter << utterances[i]
+#     end
+#   else
+#     dropout_time << i + 1 
+#   end
+# end
+
+# dropout_time.unshift(0)
+# dropout_dif = []
+
+# (dropout_time.length - 1).times do |i|
+#   dropout_dif << dropout_time[i + 1] - dropout_time[i]
+# end
+
+
+# dropout_dif.each do |dif|
+#   d = dif % player.length
+#   tmp = player[d]
+#   player.delete_at(d - 1)
+#   while true
+#     player = player.rotate
+#     break if player[0] == tmp
+#   end 
+# end
+
+# player.sort!
+# puts player.length, player
+
 n, k, m = gets.split.map &:to_i
 word_list = k.times.map{gets.chomp.to_s}
 utterances = m.times.map{gets.chomp.to_s}
 
-player = [*1..n]
-dropout_num = []
+dropout_time = []
 previous_utter = []
 
 m.times do |i|
   if word_list.include?(utterances[i]) && !(utterances[i].end_with?("z"))
     if previous_utter.include?(utterances[i])
-      dropout_num << i + 1
+      dropout_time << i + 1
     else
       previous_utter << utterances[i]
     end
   else
-    dropout_num << i + 1 
+    dropout_time << i + 1 
   end
 end
 
-dropout_num.unshift(0)
-dropout_dif = []
+now = 0
+alive = Array.new(n, true)
+a_num = 0
 
-(dropout_num.length - 1).times do |i|
-  dropout_dif << dropout_num[i + 1] - dropout_num[i]
+m.times do 
+  now += 1
+  a_num = a_num % alive.count(true)
+  if dropout_time.include?(now)
+    while true
+      break if alive[a_num] == true
+      a_num += 1
+    end
+    alive[a_num] = false
+  end
+  a_num += 1
+  a_num = a_num % n
 end
 
-
-dropout_dif.each do |dif|
-  d = dif % player.length
-  tmp = player[d]
-  player.delete_at(d - 1)
-  while true
-    player = player.rotate
-    break if player[0] == tmp
-  end 
-end
-
-player.sort!
-puts player.length, player
+p alive
