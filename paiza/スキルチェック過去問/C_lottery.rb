9cb25c06337f94c8557187657e70c1c2a17e07e8
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-24 09:48:02 +0900

    C_lottery_as

--- paiza/スキルチェック過去問/C_lottery.rb
+++ paiza/スキルチェック過去問/C_lottery.rb
@@ -1,21 +1,40 @@
-first = gets.chomp.to_s
+# first = gets.chomp.to_s
+# n = gets.to_i
+
+# n.times do
+#   num = gets.chomp.to_s
+#   if num == first
+#     puts "first"
+#   elsif num == (first.to_i + 1).to_s || num == (first.to_i - 1).to_s
+#     puts "adjacent"
+#   else
+#     last_four = first.slice(2..5)
+#     num = num.slice(2..5)
+#     if num == last_four
+#       puts "second"
+#       next
+#     end
+#     last_three = first.slice(3..5)
+#     num = num.slice(1..3)
+#     puts num == last_three ? "third" : "blank"
+#   end
+# end
+
+first = gets.to_i
 n = gets.to_i
 
 n.times do
-  num = gets.chomp.to_s
+  num = gets.to_i
+  
   if num == first
     puts "first"
-  elsif num == (first.to_i + 1).to_s || num == (first.to_i - 1).to_s
+  elsif num == first + 1 || num == first - 1
     puts "adjacent"
+  elsif num % 10000 == first % 10000
+    puts "second"
+  elsif num % 1000 == first % 1000
+    puts "third"
   else
-    last_four = first.slice(2..5)
-    num = num.slice(2..5)
-    if num == last_four
-      puts "second"
-      next
-    end
-    last_three = first.slice(3..5)
-    num = num.slice(1..3)
-    puts num == last_three ? "third" : "blank"
+    puts "blank"
   end
 end
