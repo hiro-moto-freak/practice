f436949a2b69761a87242a2283ecd23a5783eb57
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-09-03 10:12:42 +0900

    C_camp_step2

---
+++ paiza/Aランクレベルアップ/陣取りゲーム/C_camp_step2.rb
@@ -0,0 +1,20 @@
+h, w = gets.split.map &:to_i
+field = h.times.map{gets.chomp}
+
+py, px = 0, 0
+
+h.times do |y|
+  w.times do |x|
+    if field[y][x] == "*"
+      py, px = y, x
+      break
+    end
+  end
+end
+
+field[py + 1][px] = "*" if py != h - 1 && field[py + 1][px] != "#"
+field[py - 1][px] = "*" if py != 0 && field[py - 1][px] != "#"
+field[py][px + 1] = "*" if px != w - 1 && field[py][px + 1] != "#"
+field[py][px - 1] = "*" if px != 0 && field[py][px - 1] != "#"
+
+puts field
